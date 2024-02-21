407bfcb500fe30fac4400d491b92966698dcf857
Darya Krasnykh <krasnykh.daria@gmail.com>
2024-02-21 20:34:01 +0300

    Revert "test"
    This reverts commit 760431d397d400213dd916262f02c150a6be69ea.

--- cmd/gophermart/internal/storage/postgres.go
+++ cmd/gophermart/internal/storage/postgres.go
@@ -3,14 +3,18 @@ package storage
 import (
 	"context"
 	"fmt"
-	
+	"time"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/dkrasnykh/praktikum-diploma/cmd/gophermart/internal/config"
 )
 
 func New(cfg *config.Config) (*pgxpool.Pool, error) {
-	pool, err := pgxpool.New(context.Background(), cfg.DatabaseURL)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(cfg.ConnectTimeout)*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, cfg.DatabaseURL)
 	if err != nil {
 		return nil, fmt.Errorf("postgres connection error: %w", err)
 	}
